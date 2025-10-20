8a94bd39fb5f4d41089df56e1bdea70f45511dd8
sib45 <habsibnas45@gmail.com>
2025-10-20 11:29:16 +0100

    Further Setup 1

---
+++ temporary_server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 9000;
+
+//Serves static files from the 'public' directory
+app.use(express.static('public'));
+
+//Test API endpoint
+app.get('/api/test', (req, res) => {
+    res.json({message: 'API Test'})
+});
+
+//Starts the server
+app.listen(port, () => {
+    console.log(`Temporary server running at http://localhost:${port}`);
+});
