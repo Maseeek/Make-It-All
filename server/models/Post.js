f168164eb2cd1952413078c1efe4515df0379ad7
sib45 <habsibnas45@gmail.com>
2025-11-02 20:02:39 +0000

    Model Edit Request (#4)
    * Forum Models Update - 30-10-2025
    
    * Duplicate Implementation Removal
    
    * Basic Database Testing Setup
    
    * Working Database Prototyping Setup
    
    * SQLite Implementation & MySQL Server Prototypes
    
    * MySQL Implementation (using sci-project)
    
    * bugfix & cleanup
    
    * MySQL DML Implementation & DDL Fixes
     SQL DDL code was edited to properly run on MySQL
     SQL DML code examples have been successfully implemented

--- server/models/Post.js
+++ server/models/Post.js
@@ -11,4 +11,16 @@
 // - canBeShared (boolean)
 // - createdAt, updatedAt
 
+//DDL: 
+// CREATE TABLE IF NOT EXISTS tblPost (
+//     PostID INT PRIMARY KEY AUTO_INCREMENT,
+//     TopicID INT NOT NULL,
+//     Content TEXT NOT NULL,
+//     AuthorID INT NOT NULL,
+//     IsEdited BOOLEAN DEFAULT FALSE,
+//     CanBeShared BOOLEAN DEFAULT TRUE,
+//     CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+//     UpdatedAt DATETIME DEFAULT NULL
+// );
+
 export default {};
